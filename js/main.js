e656d7bc4b3ba7d057d140c1bb280589492a917d
firmo holanda <firmo@viize.com>
2020-09-18 13:10:18 -0300

    add smooth scroll

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/* jquery smooth scroll */
+$(document).on('click', 'a[href^="#"]', function (event) {
+  event.preventDefault();
+
+  $('html, body').animate({
+    scrollTop: $($.attr(this, 'href')).offset().top
+  }, 1500);
+});
