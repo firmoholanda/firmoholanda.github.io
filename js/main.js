d6577d618131378e244bb3e367f1ad5ad4894356
Firmo Holanda <firmo@viize.com>
2020-09-18 14:19:58 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-/* jquery smooth scroll */
-$(document).on('click', 'a[href^="#"]', function (event) {
-  event.preventDefault();
 
-  $('html, body').animate({
-    scrollTop: $($.attr(this, 'href')).offset().top
-  }, 1500);
-});
