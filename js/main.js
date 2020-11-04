a52c52143577ca210b5a6ae908817d728c329027
firmo holanda <firmo@viize.com>
2020-11-04 15:35:35 -0300

    change text to lower case

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* jquery smooth scroll */
+/* jquery smooth scroll
 $(document).on('click', 'a[href^="#"]', function (event) {
   event.preventDefault();
 
@@ -6,3 +6,25 @@ $(document).on('click', 'a[href^="#"]', function (event) {
     scrollTop: $($.attr(this, 'href')).offset().top
   }, 1500);
 });
+*/
+
+
+function scrollToAnchor(anchor) {
+  var aTag = document.getElementById("faq");
+  
+  console.log(anchor)
+  console.log(aTag)
+
+
+  window.scrollTo(0, 5000);
+  
+  /* 
+  window.scrollTo({
+    top: anchor.offsetHeight,
+    behavior: 'smooth'
+  });
+  */
+
+  /* $('html,body').animate({ scrollTop: aTag.offset().top }, 1500); */
+
+}
